72ff1ac997af891529699d5ce092be64a8eada64
José Ángel Hernández <joseangel@Mac-mini-de-Jose.local>
2025-05-13 10:21:23 +0200

    feat: add ProductDetails draft page

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,9 @@
+const ProductDetails = () => {
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <p>Details about the product will go here.</p>
+    </div>
+  );
+};
+export default ProductDetails;
