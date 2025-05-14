c031a4edd4c89c3e9825247c0787e654fcafa022
José Ángel Hernández <joseangel@Mac-mini-de-Jose.local>
2025-05-14 12:43:22 +0200

    feat: header implementation using  cart context and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,39 @@
+import { Link, useLocation } from 'react-router-dom';
+import { useCartContext } from '../context/CartContext';
+import styles from './Header.module.css';
+
 const Header = () => {
+  const { count } = useCartContext(); // Get count from CartContext
+  const location = useLocation();
+
+  // Display breadcrumbs based on the current location
+  const displayBreadcrumbs = () => {
+    if (location.pathname === '/') return 'Inicio';
+    if (location.pathname.startsWith('/product/')) {
+      return (
+        <>
+          <Link to="/" className={styles.breadcrumbLink}>
+            Home
+          </Link>{' '}
+          / Product Details
+        </>
+      );
+    }
+    return null;
+  };
+
   return (
-    <header>
-      <h1>ITX Header</h1>
+    <header className={styles.header}>
+      <div className={styles.left}>
+        <Link to="/" className={styles.logo}>
+          ITX Shop
+        </Link>
+        <nav className={styles.breadcrumbs}>{displayBreadcrumbs()}</nav>
+      </div>
+
+      <div className={styles.cart}>
+        Cart: <span>{count}</span>
+      </div>
     </header>
   );
 };
