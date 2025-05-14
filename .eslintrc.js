6f41d7dfcaa5dc0a3db913e8c52911f4179a75f1
José Ángel Hernández <joseangel@Mac-mini-de-Jose.local>
2025-05-14 21:40:48 +0200

    fix: modification on lint configuration file since it was not working properly

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react-hooks', 'react-refresh', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+  ignorePatterns: ['dist'],
+};
